a45249ae25fd711f48e06eaf1d213999c820bbb4
David Joy <djoy@edx.org>
2019-04-26 14:35:27 -0400

    Refactoring selectors a bit to organize/take advantage of reselect composition.

---
+++ src/account-settings/components/EditableField.messages.jsx
@@ -0,0 +1,17 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  'account.settings.editable.field.password.reset.button': {
+    id: 'account.settings.editable.field.password.reset.button',
+    defaultMessage: 'Reset Password',
+    description: 'The password reset button in account settings',
+  },
+  'account.settings.editable.field.action.save': {
+    id: 'account.settings.editable.field.action.save',
+    defaultMessage: 'Save',
+    description: 'The save button on an editable field',
+  },
+});
+
+
+export default messages;
