8f1231192b04c1bf87f0e938ec390e3ea7830958
David Joy <davidjoy@users.noreply.github.com>
2019-05-21 16:48:58 -0400

    Fix duplicate i18n messages. (#57)
    Also get file perms on i18n-concat.js correct so they can be called by make.

--- src/account-settings/components/PasswordReset.jsx
+++ src/account-settings/components/PasswordReset.jsx
@@ -19,7 +19,7 @@ function PasswordReset({ email, intl, ...props }) {
       icon={<FontAwesomeIcon className="mr-2" icon={faExclamationTriangle} />}
     >
       <FormattedMessage
-        id="account.settings.editable.field.password.reset.button"
+        id="account.settings.editable.field.password.reset.button.confirmation"
         defaultMessage="We've sent a message to {email}. Click the link in the message to reset your password. Didn't receive the message? Contact {technicalSupportLink}."
         description="The password reset button in account settings"
         values={{
@@ -29,9 +29,9 @@ function PasswordReset({ email, intl, ...props }) {
               destination="https://support.edx.org/hc/en-us/articles/206212088-What-if-I-did-not-receive-a-password-reset-message-"
             >
               <FormattedMessage
-                id="account.settings.editable.field.password.reset.button.support.link"
+                id="account.settings.editable.field.password.reset.button.confirmation.support.link"
                 defaultMessage="technical support"
-                description="link text used in message: account.settings.editable.field.password.reset.button 'Contact technical support.'"
+                description="link text used in message: account.settings.editable.field.password.reset.button.confirmation 'Contact technical support.'"
               />
             </Hyperlink>
           ),
