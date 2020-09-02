1febcd5fa0c8d7ebf76f42852bb3d8d6ea98cef6
adeelehsan <adeel.ehsan@arbisoft.com>
2020-09-02 13:21:57 +0500

    renamed registration dir to logistration
    VAN-2

--- src/data/sagas.js
+++ src/data/sagas.js
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 import { saga as accountSettingsSaga } from '../account-settings';
-import { saga as registrationSaga } from '../registration';
+import { saga as registrationSaga } from '../logistration';
 
 export default function* rootSaga() {
   yield all([
