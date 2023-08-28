3f781ef05edb9e2906fd30d60d16719590aab606
Kshitij Sobti <kshitij@sobti.in>
2023-08-28 16:05:43 +0530

    feat: add support for design tokens and CSS variables

--- src/account-settings/delete-account/ConfirmationModal.jsx
+++ src/account-settings/delete-account/ConfirmationModal.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import {
   AlertModal,
-  Button, Input, ValidationFormGroup, ActionRow,
+  Button, Form, ActionRow, FormControl, FormControlFeedback, FormLabel,
 } from '@edx/paragon';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { faExclamationCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
@@ -107,22 +107,23 @@ export class ConfirmationModal extends Component {
               <PrintingInstructions />
             </p>
           </Alert>
-          <ValidationFormGroup
-            for={passwordFieldId}
-            invalid={errorType !== null}
-            invalidMessage={intl.formatMessage(invalidMessage)}
-          >
-            <label className="d-block" htmlFor={passwordFieldId}>
+          <Form.Group isInvalid={errorType !== null} controlId={passwordFieldId}>
+            <Form.Label className="d-block">
               {intl.formatMessage(messages['account.settings.delete.account.modal.enter.password'])}
-            </label>
-            <Input
+            </Form.Label>
+            <Form.Control
               name="password"
-              id={passwordFieldId}
+              // id={passwordFieldId}
               type="password"
               value={password}
               onChange={onChange}
             />
-          </ValidationFormGroup>
+            {errorType !== null && (
+            <Form.Control.Feedback type="invalid">
+              {intl.formatMessage(invalidMessage)}
+            </Form.Control.Feedback>
+            )}
+          </Form.Group>
         </div>
 
       </AlertModal>
