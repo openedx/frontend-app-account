56f04d393c1307b2022d566555c3c3a8590e507e
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2023-11-29 16:05:08 +0500

    feat: migrate enzyme with RTL

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,8 +1,3 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
-
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
