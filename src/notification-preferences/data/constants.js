efb416292637a7beebeafc6ce0adff05831f1c09
sundasnoreen12 <sundasnoreen12@gmail.com>
2025-03-13 23:01:06 +0500

    test: added test cases

--- src/notification-preferences/data/constants.js
+++ src/notification-preferences/data/constants.js
@@ -5,3 +5,14 @@ export const EMAIL_CADENCE_PREFERENCES = {
 export const EMAIL_CADENCE = 'email_cadence';
 export const EMAIL = 'email';
 export const MIXED = 'Mixed';
+export const RequestStatus = /** @type {const} */ ({
+  IN_PROGRESS: 'in-progress',
+  SUCCESSFUL: 'successful',
+  FAILED: 'failed',
+  DENIED: 'denied',
+  PENDING: 'pending',
+  CLEAR: 'clear',
+  PARTIAL: 'partial',
+  PARTIAL_FAILURE: 'partial failure',
+  NOT_FOUND: 'not-found',
+});
