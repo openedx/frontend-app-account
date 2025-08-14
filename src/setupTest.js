ece65d024c55429730984410e9bdf6e4550b89a7
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-08-14 16:42:49 -0600

    chore: renaming MockedPluginSlot

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,15 +1,15 @@
+import { mergeSiteConfig } from '@openedx/frontend-base';
+import '@testing-library/jest-dom';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import '@testing-library/jest-dom';
 import siteConfig from 'site.config';
-import { mergeSiteConfig } from '@openedx/frontend-base';
 
-import MockedPluginSlot from './tests/MockedPluginSlot';
+import MockedSlot from './tests/MockedSlot';
 
 mergeSiteConfig(siteConfig);
 
-jest.mock('@openedx/frontend-plugin-framework', () => ({
-  ...jest.requireActual('@openedx/frontend-plugin-framework'),
+jest.mock('@openedx/frontend-base', () => ({
+  ...jest.requireActual('@openedx/frontend-base'),
   Plugin: () => 'Plugin',
-  PluginSlot: MockedPluginSlot,
+  Slot: MockedSlot,
 }));
--- src/tests/MockedSlot.test.jsx
+++ src/tests/MockedSlot.test.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import MockedPluginSlot from './MockedPluginSlot';
+import MockedSlot from './MockedSlot';
 
-describe('MockedPluginSlot', () => {
+describe('MockedSlot', () => {
   it('renders mock plugin with "PluginSlot" text', () => {
-    render(<MockedPluginSlot id="test_plugin" />);
+    render(<MockedSlot id="test_plugin" />);
 
     const component = screen.getByText('PluginSlot_test_plugin');
     expect(component).toBeInTheDocument();
@@ -13,9 +12,9 @@ describe('MockedPluginSlot', () => {
   it('renders as the slot children directly if there is content within', () => {
     render(
       <div role="article">
-        <MockedPluginSlot id="test_plugin">
+        <MockedSlot id="test_plugin">
           <q role="note">How much wood could a woodchuck chuck if a woodchuck could chuck wood?</q>
-        </MockedPluginSlot>
+        </MockedSlot>
       </div>,
     );
 
@@ -24,7 +23,7 @@ describe('MockedPluginSlot', () => {
     const slot = component.querySelector('[data-testid="test_plugin"]');
     expect(slot).toBeInTheDocument();
     expect(slot).toHaveTextContent('PluginSlot_test_plugin');
-    // Check if the quote is a direct child of the MockedPluginSlot
+    // Check if the quote is a direct child of the MockedSlot
     const quote = slot.querySelector('q');
     expect(quote).toBeInTheDocument();
     expect(quote).toHaveTextContent('How much wood could a woodchuck chuck if a woodchuck could chuck wood?');
@@ -33,9 +32,9 @@ describe('MockedPluginSlot', () => {
 
   it('renders mock plugin with a data-testid ', () => {
     render(
-      <MockedPluginSlot id="guybrush">
+      <MockedSlot id="guybrush">
         <q role="note">I am selling these fine leather jackets.</q>
-      </MockedPluginSlot>,
+      </MockedSlot>,
     );
 
     const component = screen.getByTestId('guybrush');
