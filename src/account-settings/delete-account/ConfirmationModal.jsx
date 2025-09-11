e71fcca682ab0e23d683b7d1039f1fcd6bd26f9b
Awais Ansari <79941147+awais-ansari@users.noreply.github.com>
2025-09-11 21:55:42 +0500

    Revert "Replace of injectIntl with useIntl() 9/10"

--- src/account-settings/delete-account/ConfirmationModal.jsx
+++ src/account-settings/delete-account/ConfirmationModal.jsx
@@ -1,10 +1,11 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {
   AlertModal,
   Button, Form, ActionRow,
 } from '@openedx/paragon';
-import { useIntl } from '@edx/frontend-platform/i18n';
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { faExclamationCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { getConfig } from '@edx/frontend-platform';
@@ -12,20 +13,12 @@ import messages from './messages';
 import Alert from '../Alert';
 import PrintingInstructions from './PrintingInstructions';
 
-export const ConfirmationModal = ({
-  status = null,
-  errorType = null,
-  onCancel,
-  onChange,
-  onSubmit,
-  password,
-}) => {
-  const intl = useIntl();
+export class ConfirmationModal extends Component {
   /**
    * @returns String The message id for a short description of the error, suitable for a header or
    * as the error message under an input field.
    */
-  const getShortErrorMessageId = (reason) => {
+  getShortErrorMessageId(reason) {
     switch (reason) {
       case 'empty-password':
         return 'account.settings.delete.account.error.no.password';
@@ -34,13 +27,15 @@ export const ConfirmationModal = ({
       default:
         return 'account.settings.delete.account.error.unable.to.delete';
     }
-  };
+  }
 
-  const renderError = (reason) => {
+  renderError(reason) {
+    const { errorType, intl } = this.props;
+
     if (errorType === null) {
       return null;
     }
-    const headerMessageId = getShortErrorMessageId(errorType);
+    const headerMessageId = this.getShortErrorMessageId(errorType);
     const detailsMessageId = reason === 'empty-password'
       ? null
       : 'account.settings.delete.account.error.unable.to.delete.details';
@@ -56,86 +51,103 @@ export const ConfirmationModal = ({
         ) : null}
       </Alert>
     );
-  };
+  }
 
-  const open = ['confirming', 'pending', 'failed'].includes(status);
-  const passwordFieldId = 'passwordFieldId';
-  const invalidMessage = messages[getShortErrorMessageId(errorType)];
+  render() {
+    const {
+      status,
+      errorType,
+      intl,
+      onCancel,
+      onChange,
+      onSubmit,
+      password,
+    } = this.props;
+    const open = ['confirming', 'pending', 'failed'].includes(status);
+    const passwordFieldId = 'passwordFieldId';
+    const invalidMessage = messages[this.getShortErrorMessageId(errorType)];
 
-  // TODO: We lack a good way of providing custom language for a particular site.  This is a hack
-  // to allow edx.org to fulfill its business requirements.
-  const deleteAccountModalText2MessageKey = getConfig().SITE_NAME === 'edX'
-    ? 'account.settings.delete.account.modal.text.2.edX'
-    : 'account.settings.delete.account.modal.text.2';
+    // TODO: We lack a good way of providing custom language for a particular site.  This is a hack
+    // to allow edx.org to fulfill its business requirements.
+    const deleteAccountModalText2MessageKey = getConfig().SITE_NAME === 'edX'
+      ? 'account.settings.delete.account.modal.text.2.edX'
+      : 'account.settings.delete.account.modal.text.2';
 
-  return (
-    <AlertModal
-      isOpen={open}
-      title={intl.formatMessage(messages['account.settings.delete.account.modal.header'])}
-      onClose={onCancel}
-      isOverflowVisible
-      footerNode={(
-        <ActionRow>
-          <Button variant="link" onClick={onCancel}>Cancel</Button>
-          <Button variant="danger" onClick={onSubmit}>Yes, Delete</Button>
-        </ActionRow>
+    return (
+      <AlertModal
+        isOpen={open}
+        title={intl.formatMessage(messages['account.settings.delete.account.modal.header'])}
+        onClose={onCancel}
+        isOverflowVisible
+        footerNode={(
+          <ActionRow>
+            <Button variant="link" onClick={onCancel}>Cancel</Button>
+            <Button variant="danger" onClick={onSubmit}>Yes, Delete</Button>
+          </ActionRow>
         )}
-    >
-      <div className="p-3">
-        {renderError()}
-        <Alert
-          className="alert-warning mt-n2"
-          icon={<FontAwesomeIcon className="mr-2" icon={faExclamationTriangle} />}
-        >
-          <h6>
-            {intl.formatMessage(
-              messages['account.settings.delete.account.modal.text.1'],
-              { siteName: getConfig().SITE_NAME },
+      >
+        <div className="p-3">
+          {this.renderError()}
+          <Alert
+            className="alert-warning mt-n2"
+            icon={<FontAwesomeIcon className="mr-2" icon={faExclamationTriangle} />}
+          >
+            <h6>
+              {intl.formatMessage(
+                messages['account.settings.delete.account.modal.text.1'],
+                { siteName: getConfig().SITE_NAME },
+              )}
+            </h6>
+            <p>
+              {intl.formatMessage(
+                messages[deleteAccountModalText2MessageKey],
+                { siteName: getConfig().SITE_NAME },
+              )}
+            </p>
+            <p>
+              <PrintingInstructions />
+            </p>
+          </Alert>
+          <Form.Group
+            for={passwordFieldId}
+            isInvalid={errorType !== null}
+          >
+            <Form.Label className="d-block" htmlFor={passwordFieldId}>
+              {intl.formatMessage(messages['account.settings.delete.account.modal.enter.password'])}
+            </Form.Label>
+            <Form.Control
+              name="password"
+              id={passwordFieldId}
+              type="password"
+              value={password}
+              onChange={onChange}
+            />
+            {errorType !== null && (
+              <Form.Control.Feedback type="invalid" feedback-for={passwordFieldId}>
+                {intl.formatMessage(invalidMessage)}
+              </Form.Control.Feedback>
             )}
-          </h6>
-          <p>
-            {intl.formatMessage(
-              messages[deleteAccountModalText2MessageKey],
-              { siteName: getConfig().SITE_NAME },
-            )}
-          </p>
-          <p>
-            <PrintingInstructions />
-          </p>
-        </Alert>
-        <Form.Group
-          for={passwordFieldId}
-          isInvalid={errorType !== null}
-        >
-          <Form.Label className="d-block" htmlFor={passwordFieldId}>
-            {intl.formatMessage(messages['account.settings.delete.account.modal.enter.password'])}
-          </Form.Label>
-          <Form.Control
-            name="password"
-            id={passwordFieldId}
-            type="password"
-            value={password}
-            onChange={onChange}
-          />
-          {errorType !== null && (
-          <Form.Control.Feedback type="invalid" feedback-for={passwordFieldId}>
-            {intl.formatMessage(invalidMessage)}
-          </Form.Control.Feedback>
-          )}
-        </Form.Group>
-      </div>
+          </Form.Group>
+        </div>
 
-    </AlertModal>
-  );
-};
+      </AlertModal>
+    );
+  }
+}
 
 ConfirmationModal.propTypes = {
   status: PropTypes.oneOf(['confirming', 'pending', 'deleted', 'failed']),
   errorType: PropTypes.oneOf(['empty-password', 'server']),
+  intl: intlShape.isRequired,
   onCancel: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   password: PropTypes.string.isRequired,
 };
 
-export default ConfirmationModal;
+ConfirmationModal.defaultProps = {
+  status: null,
+  errorType: null,
+};
+
+export default injectIntl(ConfirmationModal);
--- src/account-settings/delete-account/DeleteAccount.test.jsx
+++ src/account-settings/delete-account/DeleteAccount.test.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-no-useless-fragment */
+import React from 'react';
 import renderer from 'react-test-renderer';
-import { IntlProvider } from '@edx/frontend-platform/i18n';
+import { IntlProvider, injectIntl } from '@edx/frontend-platform/i18n';
 
 // Testing the modals separately, they just clutter up the snapshots if included here.
 jest.mock('./ConfirmationModal', () => function ConfirmationModalMock() {
@@ -10,21 +11,20 @@ jest.mock('./SuccessModal', () => function SuccessModalMock() {
   return <></>;
 });
 
-jest.mock('./data/actions', () => ({
-  deleteAccount: jest.fn(),
-  deleteAccountConfirmation: jest.fn(),
-  deleteAccountFailure: jest.fn(),
-  deleteAccountReset: jest.fn(),
-  deleteAccountCancel: jest.fn(),
-}));
-
 import { DeleteAccount } from './DeleteAccount'; // eslint-disable-line import/first
+
+const IntlDeleteAccount = injectIntl(DeleteAccount);
 
 describe('DeleteAccount', () => {
   let props = {};
 
   beforeEach(() => {
     props = {
+      deleteAccount: jest.fn(),
+      deleteAccountConfirmation: jest.fn(),
+      deleteAccountFailure: jest.fn(),
+      deleteAccountReset: jest.fn(),
+      deleteAccountCancel: jest.fn(),
       status: null,
       errorType: null,
       hasLinkedTPA: false,
@@ -36,7 +36,7 @@ describe('DeleteAccount', () => {
     const tree = renderer
       .create((
         <IntlProvider locale="en">
-          <DeleteAccount
+          <IntlDeleteAccount
             {...props}
           />
         </IntlProvider>
@@ -50,7 +50,7 @@ describe('DeleteAccount', () => {
     const tree = renderer
       .create((
         <IntlProvider locale="en">
-          <DeleteAccount
+          <IntlDeleteAccount
             {...props}
             isVerifiedAccount={false}
           />
@@ -64,7 +64,7 @@ describe('DeleteAccount', () => {
     const tree = renderer
       .create((
         <IntlProvider locale="en">
-          <DeleteAccount
+          <IntlDeleteAccount
             {...props}
             hasLinkedTPA
           />
